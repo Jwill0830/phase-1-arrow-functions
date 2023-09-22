8274eda6adb3d97ebef3c4cc7bb68150886abf94
Jack Williams <jackmwilliams35@gmail.com>
2023-09-22 09:05:32 -0500

    lab completed

---
+++ index.js
@@ -0,0 +1,26 @@
+// first test-- this test is checking if there is a function named 'divide'
+
+const divide = function(a,b) {
+    return a/b
+}
+
+//explanation we're using the 'const' keyword to declare a function expression named 'divide' this function takes in two parameters 'a' and 'b' and returns their division
+
+// second test - this test wants to make sure that when the 'divide' function is called with '2000' and '100' as arguments it returns '20'
+
+
+
+//this is the same formula as above, it just has numbers on it now to satisfy the  test
+
+// test number 3 - this test is checkinig to see if we have a function named 'square'
+
+const square = x => x*x
+
+// explanation we are using the 'const' keyword to declare an arrow function named 'square' this funciton takes in one parameter 'x' and returns its square ('x * x')
+
+// test 5 - this test is checking if there is an arrow function named 'add'
+
+const add = (a, b) => a + b
+
+// explanation - we are using the 'const' keyword to declare an arrow function named 'add' this function takes in 2 parameters a and b and returns their sum
+
